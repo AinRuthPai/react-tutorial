419c0af9c79c44c1ba5cac10d2cad9a1e2cce4df
AinRuthPai <tanbing@naver.com>
2022-05-19 23:27:00 +0900

    1장 22번 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,93 @@
-import { useCallback, useMemo, useRef, useState } from "react";
+import React, { useCallback, useMemo, useRef, useState, useReducer } from "react";
 import "./App.css";
+import Counter from "./components/Counter";
 import CreateUser from "./components/CreateUser";
 import UserList from "./components/UserList";
+import useInputs from "./hooks/useInputs";
 
+function countActiveUsers(users) {
+  console.log("활성 사용자 수를 세는 중...");
+  return users.filter((user) => user.active).length;
+}
+
+const initialState = {
+  inputs: {
+    username: "",
+    email: "",
+  },
+
+  users: [
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+      active: true,
+    },
+    {
+      id: 2,
+      username: "velopert2",
+      email: "public.velopert2@gmail.com",
+      active: false,
+    },
+    {
+      id: 3,
+      username: "velopert3",
+      email: "public.velopert3@gmail.com",
+      active: false,
+    },
+  ],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CHANGE_INPUT":
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value,
+        },
+      };
+    case "CREATE_USER":
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user),
+      };
+    case "TOGGLE_USER":
+      return {
+        ...state,
+        users: state.users.map((user) => {
+          return user.id === action.id ? { ...user, active: !user.active } : user;
+        }),
+      };
+    case "REMOVE_USER":
+      return {
+        ...state,
+        users: state.users.filter((user) => user.id !== action.id),
+      };
+    default:
+      return state;
+  }
+}
+
+export const UserDispatch = React.createContext(null);
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { users } = state;
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
+  return (
+    <UserDispatch.Provider value={dispatch}>
+      <CreateUser />
+      <UserList users={users} />
+      <div>활성 사용자 수 : {count} </div>
+    </UserDispatch.Provider>
+  );
+}
+
+/* // useReducer 사용 전 코드
 function countActiveUsers(users) {
   // useMemo를 사용하지 않으면 input의 값을 바꿀때에도 이 함수가 호출된다.
   console.log("활성 사용자 수를 세는 중...");
@@ -81,11 +166,12 @@ function App() {
 
   return (
     <div className='App'>
+      <Counter />
       <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
       <div>활성 사용자 수 : {count} </div>
     </div>
   );
 }
-
+*/
 export default App;
